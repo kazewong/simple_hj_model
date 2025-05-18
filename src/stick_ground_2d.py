f1cd9e7e0857277965db4c58549239ebb1a9b4da
kazewong <kazewong.physics@gmail.com>
2025-05-18 09:54:07 -0400

    Refactor solve_system function and add numpy import for improved calculations

--- src/stick_ground_2d.py
+++ src/stick_ground_2d.py
@@ -1,4 +1,5 @@
 import sympy as sp
+import numpy as np
 
 # Define variables
 M, m, ux, uy, omega_init, L, theta, mu = sp.symbols("M m ux uy omega_init L theta mu")
@@ -32,17 +33,65 @@ f4 = (
     - (I_hat * omega_after + m * L * sp.cos(theta) * vy - m * L * sp.sin(theta) * vx)
 )
 f5 = (
-    I_tilde * omega_init + m * uy * L_prime - (I_tilde * omega_after + m * L_prime * vy)
+    I_tilde * omega_init + m * ux * L_prime - (I_tilde * omega_after + m * L_prime * vx)
 )
 
 equation_sets = [f1, f2, f3, f4, f5]
 
+
+def solve_system(
+    ux_value,
+    uy_value,
+    omega_init_value,
+    theta_value,
+    mu_value,
+    M_value = 10000000,
+    m_value = 1,
+    L_value = 1,
+):
+    # Substitute concrete values
+    subs_dict = {
+        M: M_value,
+        m: m_value,
+        ux: ux_value,
+        uy: uy_value,
+        omega_init: omega_init_value,
+        L: L_value,
+        theta: theta_value,
+        mu: mu_value,
+    }
+    concrete_eqs = [eq.subs(subs_dict) for eq in equation_sets]
+
+    # Solve the system
+    # result = sp.nsolve(
+    #     concrete_eqs, (Vx, Vy, vx, vy, omega_after), (0, 0, 100.0, 100.0, 100.0), dict=True
+    # )
+    result = sp.solve(concrete_eqs, (Vx, Vy, vx, vy, omega_after), dict=True)
+    print("Result: ", result)
+
+    test_result = [eq.subs(result[-1]).evalf() for eq in concrete_eqs]
+    for i, eq in enumerate(test_result):
+        if abs(eq) > 1e-5:
+            print(f"Equation {i+1} not satisfied: {eq}")
+
+    return {
+        "vx": result[0][vx],
+        "vy": result[0][vy],
+        "omega_after": result[0][omega_after],
+    }
+
+
 # Substitute concrete values
-subs_dict = {M: 1000000, m: 1, ux: 1, uy: 0.1, omega_init: 0.1, L: 1, theta: 80, mu: 1}
-concrete_eqs = [eq.subs(subs_dict) for eq in equation_sets]
+subs_dict = {
+    "M_value": 10000000,
+    "m_value": 1,
+    "ux_value": -1.0,
+    "uy_value": 0.0,
+    "omega_init_value": 0.1,
+    "L_value": 1,
+    "theta_value": np.deg2rad(70),
+    "mu_value": 1,
+}
 
-print("Solving the following equations:")
-# Solve the system
-result = sp.solve(concrete_eqs, [Vx, Vy, vx, vy, omega_after], dict=True)
-
-print(result)
+result = solve_system(**subs_dict)
+print("Result: ", result)
