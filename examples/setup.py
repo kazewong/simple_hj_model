b0d9ec88404b0904d330db8fdbaa53055d88ea7a
Kaze Wong <kazewong.physics@gmail.com>
2025-09-26 21:36:26 -0400

    Add example to combine multisegment model

--- examples/setup.py
+++ examples/setup.py
@@ -5,7 +5,11 @@ pit = mujoco.MjSpec.from_file('./src/xmls/pit.xml')
 pit.modelname = "pit"
 
 rod = mujoco.MjSpec.from_file('./src/xmls/rod.xml')
+rod.modelname = "rod"
+
+multisegment = mujoco.MjSpec.from_file('./src/xmls/multi-segment.xml')
 
 world = pit.attach(rod,frame=pit.frames[0], prefix='child-')
+world = pit.attach(multisegment, frame=pit.frames[0], prefix='multi-')
 
 model = pit.compile()
