7769fa643b4d3f792c94c8ccee21cf62946eb78a
Kaze Wong <kazewong.physics@gmail.com>
2025-09-28 21:33:54 -0400

    Scaffolding simulator

---
+++ examples/run_simulation.py
@@ -0,0 +1,20 @@
+import mujoco
+import mujoco.viewer as viewer
+from hjsimulator.models.pit import PitModel
+from hjsimulator.models.rod import RodModel
+from hjsimulator.models.multisegment import MultiSegmentModel
+
+pit = PitModel()
+rod = RodModel(length=2.0)
+multisegment = MultiSegmentModel()
+
+world = pit.spec.attach(rod.spec,frame=pit.spec.frames[0], prefix='child-')
+world = pit.spec.attach(multisegment.spec, frame=pit.spec.frames[0], prefix='multi-')
+
+
+model = pit.spec.compile()
+data = mujoco.MjData(model)
+
+print(data.qpos)
+mujoco.mj_step(model, data)
+print(data.qpos)
